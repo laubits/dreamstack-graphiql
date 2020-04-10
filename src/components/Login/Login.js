d89ab69ef4fc9af623a65c697fc41d8b673116a8
gaboesquivel <contact@gaboesquivel.com>
2020-04-10 10:20:28 -0600

    fix: update graphql url

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import './App.css';
-import { updateGraphQLEndpoint } from '../Services/ApiExplorer/Actions';
+import React from "react";
+import PropTypes from "prop-types";
+import Helmet from "react-helmet";
+import "./App.css";
+import { updateGraphQLEndpoint } from "../Services/ApiExplorer/Actions";
 
-const isProd = window.location.hostname.includes('graph.telosdreamstack');
-const graphqlEndpoint = isProd ?
-  'http://mainnet.graph.telosdreamstack.io/v1/graphql' :
-  'http://hasura.local.telosdreamstack.io/v1/graphql';
+const isProd = window.location.hostname.includes("graph.telosdreamstack");
+const graphqlEndpoint = isProd
+  ? "http://mainnet.telosdreamstack.io/v1/graphql"
+  : "http://hasura.local.telosdreamstack.io/v1/graphql";
 
 console.log(`LOGIN graphqlEndpoint=${graphqlEndpoint}`);
 
 class LoginComponent extends React.Component {
   constructor() {
     super();
-    this.state = { graphqlEndpoint: '' };
+    this.state = { graphqlEndpoint: "" };
   }
   setGraphQLEndpoint(e) {
     this.setState({ graphqlEndpoint: e.target.value });
@@ -43,23 +43,27 @@ class LoginComponent extends React.Component {
                 <button
                   className="loginButton"
                   type="submit"
-                  onClick={(e) => {
+                  onClick={e => {
                     e.preventDefault();
-                    const urlRegex= /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
+                    const urlRegex = /^(http[s]?:\/\/){0,1}(www\.){0,1}[a-zA-Z0-9\.\-]+\.[a-zA-Z]{2,5}[\.]{0,1}/;
                     if (!urlRegex.test(this.state.graphqlEndpoint)) {
                       // check if localhost url
                       const localhostRegex = /^http:\/\/\w+(\.\w+)*(:[0-9]+)?\/?(\/[.\w]*)*$/;
-                      if(!localhostRegex.test(this.state.graphqlEndpoint)) {
-                        alert('Please enter a valid URL');
+                      if (!localhostRegex.test(this.state.graphqlEndpoint)) {
+                        alert("Please enter a valid URL");
                       } else {
-                        dispatch(updateGraphQLEndpoint(this.state.graphqlEndpoint));
+                        dispatch(
+                          updateGraphQLEndpoint(this.state.graphqlEndpoint)
+                        );
                       }
                     } else {
-                      dispatch(updateGraphQLEndpoint(this.state.graphqlEndpoint));
+                      dispatch(
+                        updateGraphQLEndpoint(this.state.graphqlEndpoint)
+                      );
                     }
                   }}
                 >
-                  <i className={'fa fa-sign-in'} />
+                  <i className={"fa fa-sign-in"} />
                 </button>
               </form>
             </div>
@@ -67,10 +71,16 @@ class LoginComponent extends React.Component {
         </div>
         <div className="footerWrapper">
           <div className="built">
-            Built with <i className="fa fa-heart" /> by <a href={'http://hasura.io/'} target={'_blank'}>Hasura</a>
+            Built with <i className="fa fa-heart" /> by{" "}
+            <a href={"http://hasura.io/"} target={"_blank"}>
+              Hasura
+            </a>
           </div>
           <div className="apiHasura">
-            <a href="https://github.com/hasura/graphql-engine/tree/master/community/tools/graphiql-online" target={'_blank'}>
+            <a
+              href="https://github.com/hasura/graphql-engine/tree/master/community/tools/graphiql-online"
+              target={"_blank"}
+            >
               <i className="fa fa-github" />
             </a>
           </div>
@@ -81,17 +91,16 @@ class LoginComponent extends React.Component {
 }
 
 LoginComponent.propTypes = {
-  dispatch: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired
 };
 
 const generatedLoginComponent = connect => {
   const mapStateToProps = state => {
     return {
-      ...state.apiexplorer,
+      ...state.apiexplorer
     };
   };
   return connect(mapStateToProps)(LoginComponent);
 };
 
 export default generatedLoginComponent;
-
